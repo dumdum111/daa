562f605beb368b85c7db7337fb21379c8c3af5a6
dumdum111 <brosyco6@gmail.com>
2024-07-30 07:22:51 +0530

    Update 5.cpp

--- 5.cpp
+++ 5.cpp
@@ -1,15 +1,15 @@
 #include<stdio.h>
 #include<string.h>
 int horspool(char T[100], char P[100], int n, int m) {
-    char ST[200];
-    for(int i = 0; i < n; i++) ST[T[i]] = m;
-    for(int i = 0; i < m - 1; i++) ST[P[i]] = m - 1 - i;
+    char mp[256];
+    for(int i = 0; i < 256; i++) mp[i] = m;
+    for(int i = 0; i < m - 1; i++) mp[(unsigned char)P[i]] = m - 1 - i;
     int i = m - 1;
     while(i < n) {
         int j = 0;
         while(j < m && (P[m - 1 - j] == T[i - j])) j++;
         if(j == m) return (i - m + 1);
-        else i += ST[T[i]];
+        else i += mp[(unsigned char)T[i]];
     }
     return -1;
 }
@@ -21,6 +21,8 @@ int main() {
     printf("Enter the pattern string:\n");
     fgets(P, sizeof(P), stdin);
     int n = strlen(T), m = strlen(P);
+    if(T[n - 1] == '\n') T[n - 1] = '\0';
+    if(P[m - 1] == '\n') P[m - 1] = '\0';
     int flag = horspool(T, P, n, m);
     if(flag == -1) printf("String not found!\n");
     else printf("String found from index %d.", flag);
